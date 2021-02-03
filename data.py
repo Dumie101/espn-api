8dad13f224971f10851f2da9999cd58c24c8af12
Dumie101 <36200378+Dumie101@users.noreply.github.com>
2021-02-02 23:20:59 -0400

    Update data.py

--- data.py
+++ data.py
@@ -1,12 +1,21 @@
-from main import *
-
+import string
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup
+from main import playersDic, get_key
+from nameMaker import nameCreator
 
-URL = 'https://www.basketball-reference.com/players/a/adamsst01/gamelog/2021/'
 
+for i in sorted(playersDic.values(), reverse=True):
+    name, first = nameCreator(
+        get_key(i).__getattribute__('name').lower().translate(str.maketrans('', '', string.punctuation)))
+    print(name, first)
+
+
+URL = 'https://www.basketball-reference.com/players/h/hardeja01/gamelog/2021'
 page = requests.get(URL)
+
+
 
 soup = BeautifulSoup(page.content, 'html.parser')
 
@@ -14,9 +23,15 @@ headers = [th.getText() for th in soup.findAll('thead', limit=10)[0].findAll('th
 
 headers = headers[1:]
 
-# avoid the first header row
-rows = soup.findAll('tr')
 
+rows = soup.findAll('tr')
 
 player_stats = [[td.getText() for td in rows[i].findAll('td')]
                 for i in range(len(rows))]
+
+
+
+stats = pd.DataFrame(player_stats, columns=headers)
+
+print(stats)
+
